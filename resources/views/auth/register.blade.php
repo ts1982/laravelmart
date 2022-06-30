6fb4fb3bfb8015afebfcef52a5753cdd66229277
ts1982 <t.saito55555@gmail.com>
2022-07-01 00:34:31 +0900

    04 make_design

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,139 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('ユーザー登録') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="name"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('氏名') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="name" type="text"
+                                        class="form-control @error('name') is-invalid @enderror" name="name"
+                                        value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="email"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('メールアドレス') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email">
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="postal"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('郵便番号') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="postal" type="text"
+                                        class="form-control @error('postal') is-invalid @enderror" name="postal"
+                                        value="{{ old('postal') }}" required autocomplete="postal">
+
+                                    @error('postal')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="postal"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('住所') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="address" type="text"
+                                        class="form-control @error('address') is-invalid @enderror" name="address"
+                                        value="{{ old('address') }}" required autocomplete="address">
+
+                                    @error('address')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="postal"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('電話番号') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="tel" type="text"
+                                        class="form-control @error('tel') is-invalid @enderror" name="tel"
+                                        value="{{ old('tel') }}" required autocomplete="tel">
+
+                                    @error('tel')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        required autocomplete="new-password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password-confirm"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('パスワード確認') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password-confirm" type="password" class="form-control"
+                                        name="password_confirmation" required autocomplete="new-password">
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('登録') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,6 +1,55 @@
-{{ $product->name }}
-{{ $product->description }}
-{{ $product->price }}
-{{ $product->category->name }}
+@extends('layouts.app')
 
-<a href="/products">Back</a>
+@section('content')
+    <div class="d-flex justify-content-center">
+        <div class="row w-75">
+            <div class="col-5 offset-1">
+                <img src="{{ asset('img/dummy.png') }}" class="w-100 img-fluid">
+            </div>
+            <div class="col">
+                <div class="d-flex flex-column">
+                    <h1>{{ $product->name }}</h1>
+                    <p>{{ $product->description }}</p>
+                    <hr>
+                    <p class="d-flex align-items-end">￥{{ $product->price }}（税込）</p>
+                    <hr>
+                </div>
+                @auth
+                    <form action="" method="post">
+                        @csrf
+                        <div class="form-group row">
+                            <label for="quantity" class="col-2 col-form-label">数量</label>
+                            <div class="col-10">
+                                <input type="number" id="quantity" name="qty" min="1" value="1"
+                                    class="form-control w-25">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-7">
+                                <button type="submit" class="btn btn-success w-100">
+                                    <i class="fas fa-shopping-cart mr-1"></i>カートに追加
+                                </button>
+                            </div>
+                            <div class="col-5">
+                                <a href="" class="btn btn-outline-primary w-100">
+                                    <i class="fas fa-heart mr-1"></i>お気に入り
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                @endauth
+            </div>
+            <div class="offset-1 col-11">
+                <hr>
+                <h3>カスタマーレビュー</h3>
+            </div>
+        </div>
+    </div>
+    <a href="{{ route('products.edit', $product) }}">Edit</a>
+    <form action="{{ route('products.destroy', $product) }}" method="post">
+        @csrf
+        @method('delete')
+        <input type="submit" value="Delete">
+    </form>
+    <a href="/products">Back</a>
+@endsection
