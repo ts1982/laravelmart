963c64b9ce88b259824c3b34d63075553c61bd72
ts1982 <t.saito55555@gmail.com>
2022-07-01 14:12:02 +0900

    05 add header and footer

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -124,8 +124,8 @@
                             </div>
 
                             <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                <div class="col-md-6 offset-md-4 d-flex justify-content-center">
+                                    <button type="submit" class="btn btn-success w-50">
                                         {{ __('登録') }}
                                     </button>
                                 </div>
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-expand-md navbar-light shadow-sm fixed-bottom" style="background-color: #e3f2fd;">
+    <div class="container-fluid">
+        <a class="navbar-brand mx-auto" href="{{ url('/products') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+    </div>
+</nav>
